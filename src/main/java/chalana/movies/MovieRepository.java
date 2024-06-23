59e7289071f3ff1b58dba56c0f3ff9d4107ccde2
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-06-23 13:44:26 +0530

    create repository and service class

---
+++ src/main/java/chalana/movies/MovieRepository.java
@@ -0,0 +1,8 @@
+package chalana.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
